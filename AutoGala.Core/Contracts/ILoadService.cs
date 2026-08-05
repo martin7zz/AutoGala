8b79bed5c2ad80010fd005961417f8115cbd773c
Martin Evtimov <martinevtimov03@abv.bg>
2026-08-05 20:21:29 +0300

    -- Created interfaces

---
+++ AutoGala.Core/Contracts/ILoadService.cs
@@ -0,0 +1,17 @@
+﻿using Plugin.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plugin.Core.Contracts
+{
+    public interface ILoadService
+    {
+        LoadItem CreateLoad();
+        LoadItem CreateLoad(double n, double mx, double my);
+        void ResetCounter();
+
+    }
+}
