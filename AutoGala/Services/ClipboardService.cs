adcc706c27761f268cee447dc494785140802d29
Martin Evtimov <martinevtimov03@abv.bg>
2026-08-05 20:07:52 +0300

    -- Created a ClipboardService and a MainWindowService for clipboard functionality

---
+++ AutoGala/Services/ClipboardService.cs
@@ -0,0 +1,17 @@
+﻿using Plugin.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace AutoGala.Services
+{
+    public class ClipboardService : IClipboardService
+    {
+        public string GetText()
+        {
+            return Clipboard.GetText();
+        }
+    }
+}
---
+++ AutoGala/Services/MainWindowService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoGala.Services
+{
+    public class MainWindowService
+    {
+    }
+}
