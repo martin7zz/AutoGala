44ab29ad8b09d069e8f1c73e4d8376811bf9512d
Martin Evtimov <martinevtimov03@abv.bg>
2026-08-06 22:08:16 +0300

    -- Added window service

---
+++ AutoGala/Contracts/IWindowService.cs
@@ -0,0 +1,13 @@
+﻿using AutoGala.views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace AutoGala.Contracts
+{
+    public interface IWindowService
+    {
+        GalaPromptView ShowGalaPrompt();
+    }
+}
