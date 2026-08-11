f38325e93c9a61dd728cc50c92c28fd54a368292
Martin Evtimov <martinevtimov03@abv.bg>
2026-08-11 18:24:34 +0300

    -- added validation rules for the editing of grid cells -- unfinished edit cell converter

---
+++ AutoGala/Common/RequiredNumberValidationRule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Controls;
+
+namespace AutoGala.Common
+{
+    public class RequiredNumberValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (string.IsNullOrWhiteSpace(value?.ToString()))
+            {
+                return new ValidationResult(false, "Value is required");
+            }
+
+            if (!double.TryParse(value.ToString(), out _))
+            {
+                return new ValidationResult(false, "Value must be a number");
+            }
+
+            return ValidationResult.ValidResult;
+        }
+    }
+}
---
+++ AutoGala/Converters/EditLoadCellConverter.cs
@@ -0,0 +1,27 @@
+﻿using Plugin.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace AutoGala.Converters
+{
+    public class EditLoadCellConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var rowItem = values[0];
+            var selectedItem = values[1];
+            var isGridReadOnly = values[2] is bool b && b;
+
+            return isGridReadOnly || !ReferenceEquals(rowItem, selectedItem);
+
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
