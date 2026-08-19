ac48fad5a7fe6112e4a121aa59e396a751e25953
Martin Evtimov <martinevtimov03@abv.bg>
2026-08-19 15:09:12 +0300

    -- created edit state service so i can disable all the buttons in view models

---
+++ AutoGala/Contracts/IEditStateService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoGala.Contracts
+{
+    public interface IEditStateService
+    {
+        bool IsEditing { get; }
+        object? EditOwner { get; }
+
+        void StartEditing(object owner);
+        void StopEditing(object owner);
+
+        event EventHandler? EditStateChanged;
+    }
+}
