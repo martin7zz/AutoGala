7d2a007d60befdd05325e4ef811177a5ad61d324
Martin Evtimov <martinevtimov03@abv.bg>
2026-08-21 19:11:36 +0300

    -- converter for duplicate values

---
+++ AutoGala/Converters/IsInCollectionConverter.cs
@@ -0,0 +1,27 @@
+﻿using Plugin.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace AutoGala.Converters
+{
+    public class IsInCollectionConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values[0] is RebarItem item && values[1] is ObservableCollection<RebarItem> duplicates)
+            {
+                return duplicates.Contains(item);
+            }
+            return false;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
