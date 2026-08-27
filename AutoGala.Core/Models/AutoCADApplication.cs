c400576f6c1b174810b9391dbed2561ea2fb4376
Martin Evtimov <martinevtimov03@abv.bg>
2026-08-27 21:47:26 +0300

    -- created autoCAD instance model

---
+++ AutoGala.Core/Models/AutoCADApplication.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plugin.Core.Models
+{
+    public class AutoCADApplication
+    {
+        public int ProcessId { get; init; }
+        public string ProcessName { get; init; } = "";
+        public string WindowTitle { get; init; } = "";
+        public Process? Process { get; init; }
+    }
+}
