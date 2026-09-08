80023e0417888409af9da929edc01887b96319e4
Martin Evtimov <martinevtimov03@abv.bg>
2026-09-08 22:04:29 +0300

    -- created autogala settings service for custom scale factors

---
+++ AutoGala/Contracts/IAutoCADSettingsService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoGala.Contracts
+{
+    public interface IAutoCADSettingsService
+    {
+        double ScaleFactor { get; set; }
+    }
+}
---
+++ AutoGala/Services/AutoCADSettingsService.cs
@@ -0,0 +1,12 @@
+﻿using AutoGala.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoGala.Services
+{
+    internal class AutoCADSettingsService : IAutoCADSettingsService
+    {
+        public double ScaleFactor { get; set; } = 0.1;
+    }
+}
