38b30af219baab2053cc84ad181d528aae89c5b0
Martin Evtimov <martinevtimov03@abv.bg>
2026-09-08 22:09:45 +0300

    -- created a view model for the loading view

---
+++ AutoGala/ViewModels/LoadingWindowViewModel.cs
@@ -0,0 +1,29 @@
+﻿using AutoGala.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoGala.ViewModels
+{
+    public class LoadingWindowViewModel : ViewModelBase
+    {
+        private string _message;
+
+        public string Message
+        {
+            get => _message;
+            set
+            {
+                if (value == _message) return;
+
+                _message = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public LoadingWindowViewModel(string message)
+        {
+            _message = message;
+        }
+    }
+}
