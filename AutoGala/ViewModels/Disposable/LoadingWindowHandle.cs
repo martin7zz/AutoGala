70cb6a42359d6a8c384edc550b6bdcba18948f7d
Martin Evtimov <martinevtimov03@abv.bg>
2026-09-08 22:07:06 +0300

    -- disposable window handle for autocad loading

---
+++ AutoGala/ViewModels/Disposable/LoadingWindowHandle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace AutoGala.ViewModels.Disposable
+{
+    public sealed class LoadingWindowHandle : IDisposable
+    {
+        private readonly Window _window;
+
+        public LoadingWindowHandle(Window window)
+        {
+            _window = window;
+        }
+
+        public string Message
+        {
+            get => ((LoadingWindowViewModel)_window.DataContext).Message;
+            set => ((LoadingWindowViewModel)_window.DataContext).Message = value;
+        }
+
+        public void Dispose()
+        {
+            _window.Close();
+        }
+    }
+}
