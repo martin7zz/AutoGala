6987085b6ced7dba6271a14e05e055b6a45481ed
Martin Evtimov <martinevtimov03@abv.bg>
2026-09-08 22:05:14 +0300

    -- created dialog service for custom dialogue options

---
+++ AutoGala/Contracts/IDialogService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoGala.Contracts
+{
+    public interface IDialogService
+    {
+        bool Confirm(string message, string title);
+    }
+}
---
+++ AutoGala/Services/DialogService.cs
@@ -0,0 +1,21 @@
+﻿using AutoGala.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace AutoGala.Services
+{
+    public class DialogService : IDialogService
+    {
+        public bool Confirm(string message, string title)
+        {
+            return MessageBox.Show(
+                message,
+                title,
+                MessageBoxButton.YesNo,
+                MessageBoxImage.Question
+            ) == MessageBoxResult.Yes;
+        }
+    }
+}
